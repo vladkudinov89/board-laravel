668170d1d90b96010f3d7756c5feb79c0a76974d
vladkudino89 <vlad.kudinov.89@gmail.com>
2019-07-25 17:41:31 +0300

    Fix factory faker title & description.

--- src/database/factories/ProjectFactory.php
+++ src/database/factories/ProjectFactory.php
@@ -6,8 +6,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
     return [
-        'title' => $this->faker->sentence,
-        'description' => $this->faker->paragraph,
+        'title' => $this->faker->sentence(4),
+        'description' => $this->faker->paragraph(4),
         'owner_id' => function (){
             return factory(User::class)->create()->id;
         }
