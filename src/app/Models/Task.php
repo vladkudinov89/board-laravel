a4846363d833b415de07c9d3091d07b0c5c363e4
vladkudino89 <vlad.kudinov.89@gmail.com>
2019-07-26 04:21:20 +0300

    Added task's properties.

--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use App\Models\Project;
 
+/**
+ * @property string $body
+ * @property int $project_id
+ */
 class Task extends AbstractBaseModel
 {
+     protected $fillable = [
+        'body',
+        'project_id'
+    ];
+
     protected $guarded = [];
 
     public function project()
